ced13e37958e246a2da77017636f1a95013a0eb0
zmax <zmythleo@gmail.com>
2023-05-09 18:23:15 +0800

    adjust

--- src/web/path_result.js
+++ src/web/path_result.js
@@ -1,13 +1,10 @@
 export function path_result(arr) {
-    if(!window.astar) {
-        return;
-    }
-    let old = window.astar.result;
-    if(old.buffer.byteLength < arr.length * 4) {
-        old = new Uint32Array(arr.length);
-    }else{
+    let old = window._astar_path_result;
+    if(old && old.buffer.byteLength >= arr.length * 4) {
         old = new Uint32Array(old.buffer);
+    }else{
+        old = new Uint32Array(arr.length);
     }
     old.set(arr);
-    window.astar.result = old.subarray(0, arr.length);
+    window._astar_path_result = old.subarray(0, arr.length);
 }
